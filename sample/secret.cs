f1818e256c32e1d978c61d6f9dbdb168337740af
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2006-08-02 04:43:54 +0000

    2006-08-02 Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* sample/Makefile.am:
    	* sample/secret.cs: test program.
    
    	* docs/Gnome.Keyring/index.xml:
    	* docs/Gnome.Keyring/Gnome.Keyring/ItemType.xml:
    	* docs/Gnome.Keyring/Gnome.Keyring/GenericItemData.xml:
    	* docs/Gnome.Keyring/Gnome.Keyring/KeyringException.xml:
    	* docs/Gnome.Keyring/Gnome.Keyring/ItemData.xml:
    	* docs/Gnome.Keyring/Gnome.Keyring/NoteItemData.xml:
    	* docs/Gnome.Keyring/Gnome.Keyring/NetItemData.xml:
    	* docs/Gnome.Keyring/Gnome.Keyring/Ring.xml: updated docs.
    
    
    svn path=/trunk/gnome-keyring-sharp/; revision=63235

---
+++ sample/secret.cs
@@ -0,0 +1,80 @@
+//
+// secret.cs
+//
+// Authors:
+//	Gonzalo Paniagua Javier (gonzalo@ximian.com)
+//
+// (C) Copyright 2006 Novell, Inc. (http://www.novell.com)
+//
+
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections;
+using Gnome.Keyring;
+
+namespace Gnome.Keyring {
+	public class Test {
+		static void Main ()
+		{
+			if (!Ring.Available) {
+				Console.WriteLine ("The gnome-keyring-daemon cannot be reached.");
+				return;
+			}
+
+			string deflt = Ring.GetDefaultKeyring ();
+			Console.WriteLine ("The default keyring is '{0}'", deflt);
+			Console.Write ("Other rings available: ");
+
+			foreach (string s in Ring.GetKeyrings ()) {
+				if (s != deflt)
+					Console.Write ("'{0}' ", s);
+			}
+			Console.WriteLine ();
+
+			// This is equivalent to...
+			foreach (ItemData s in Ring.FindNetworkPassword ("gonzalo", null, null, null, null, null, 0)) {
+				Console.WriteLine ("HERE");
+				Console.WriteLine (s);
+			}
+
+			// ... this other search.
+			Hashtable tbl = new Hashtable ();
+			tbl ["user"] = "gonzalo";
+			foreach (ItemData s in Ring.Find (ItemType.NetworkPassword, tbl)) {
+				Console.WriteLine (s);
+			}
+
+			tbl = new Hashtable ();
+			tbl ["user"] = "lalalito";
+			tbl ["domain"] = "MiDomain";
+			Console.WriteLine ("Creating item");
+			int i = Ring.CreateItem (null, ItemType.NetworkPassword, "lala@pepe.com", tbl, "laclave", true);
+			Console.WriteLine ("Deleting it (ID = {0})", i);
+		//	Ring.DeleteItem (Ring.GetDefaultKeyring (), i);
+			Console.WriteLine ("Existing IDs...");
+			foreach (int nn in Ring.ListItemIDs (deflt)) {
+				Console.WriteLine (nn);
+			}
+		}
+	}
+}
+
